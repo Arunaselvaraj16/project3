17f7c42c952bbbccfab77f90f57c2150b4a6f2f1
Arunaselvaraj16 <142681831+Arunaselvaraj16@users.noreply.github.com>
2023-09-15 15:28:02 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,20 @@
+m1=int(input("tamil:"))
+m2=int(input("english:"))
+m3=int(input("maths:"))
+m4=int(input("chemistry:"))
+m5=int(input("physics:"))
+m6=int(input("biology:"))
+if m1>m2 and m1>m3 and m1>m4 and m1>m5 and m1>m6:
+    print("maximum mark is:",m1)
+if m2>m1 and m2>m3 and m2>m4 and m2>m5 and m2>m6:
+    print("maximum mark is:",m2) 
+if m3>m1 and m3>m2 and m3>m4 and m3>m5 and m3>m6:
+    print("maximum mark is:",m3) 
+if m4>m1 and m4>m2 and m4>m3 and m4>m5 and m4>m6:
+    print("maximum mark is:",m4) 
+if m5>m1 and m5>m2 and m5>m3 and m5>m4 and m5>m6:
+    print("maximum mark is:",m5)
+if m6>m1 and m6>m2 and m6>m3 and m6>m4 and m6>m5:
+    print("maximum mark is:",m6)    
+
+    
